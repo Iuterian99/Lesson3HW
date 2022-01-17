2315f5c3e28a4383be1b57c3f3a4cc11f63047c5
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-17 07:13:36 +0500

    finished html part

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, ".", "views", "home.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, ".", "views", "login.html"));
+});
 
 app.listen(9000, console.log(9000));
