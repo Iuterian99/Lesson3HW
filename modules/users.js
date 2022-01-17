dc1aece1d29588ba0284acb118a82f516af4999b
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-17 23:35:46 +0500

    created user.json

---
+++ modules/users.js
@@ -0,0 +1,17 @@
+[
+  {
+    id: 1,
+    name: "Muslim",
+    password: "muslim123",
+  },
+  {
+    id: 2,
+    name: "Sardor",
+    password: "sardor123",
+  },
+  {
+    id: 3,
+    name: "Shoxrux",
+    password: "shoxrux123",
+  },
+];
